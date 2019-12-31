9b4c952a76b7036c02d96c183769512860b82ac7
Paul Kulchenko <paul@kulchenko.com>
2019-12-30 16:45:59 -0800

    Updated wxlua version to 3.0.0.8.

--- wxLua/bindings/genwxbind.lua
+++ wxLua/bindings/genwxbind.lua
@@ -17,7 +17,7 @@
 -- Globals
 -- ---------------------------------------------------------------------------
 
-WXLUA_BINDING_VERSION = 36 -- Used to verify that the bindings are updated
+WXLUA_BINDING_VERSION = 37 -- Used to verify that the bindings are updated
                            -- This must match modules/wxlua/wxldefs.h
                            -- otherwise a compile time error will be generated.
 
